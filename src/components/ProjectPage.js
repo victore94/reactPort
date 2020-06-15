7a9d6297c681351f3ba30c1a200c3724f1cc6675
Victor Lopez <victorel94@gmail.com>
2020-06-14 20:11:12 -0700

    Added more projects and descriptions

--- src/components/ProjectPage.js
+++ src/components/ProjectPage.js
@@ -5,12 +5,26 @@ export class ProjectPage extends React.Component {
 
     render() {
 
+        let ebrius =
+            <a
+                className='about-link'
+                rel='noreferrer'
+                href='https://secure-plateau-53716.herokuapp.com/'>
+                Ebrius
+            </a>
         let expensify =
             <a
                 className='about-link'
                 rel='noreferrer'
                 href='https://vl-react-expensify-app.herokuapp.com/'>
                 Expensify
+            </a>
+        let gamblr =
+            <a
+                className='about-link'
+                rel='noreferrer'
+                href='http://evening-plains-03671.herokuapp.com/'>
+                Gamblr
             </a>
         let instagram =
             <a
@@ -36,26 +50,32 @@ export class ProjectPage extends React.Component {
                 <div className='content-container__projects' >
                     <h3> {expensify}
                         <p>
-                            this is where the description will be.
-                            I will write a description for each of the
-                            projects and perhaps write down the tech used for each
-                            of them
-                    </p>
-                    </h3>
-                    <h3> {twitch}
-                        <p>
-                            this is where the description will be.
-                            I will write a description for each of the
-                            projects and perhaps write down the tech used for each
-                            of them
+                            ‘Expensify’ allows a user to login with a Google account and input expenses or bills you may have.
+                            The application quickly tallies up your total costs and allows a user to search expenses as well.
+                            This app utilizes React, SCSS, Firebase, and Redux.
                     </p>
                     </h3>
                     <h3> {instagram}
                         <p>
-                            this is where the description will be.
-                            I will write a description for each of the
-                            projects and perhaps write down the tech used for each
-                            of them
+                            'Instagram Bot' is an automated web-scraper that logs into Instagram and follows the most popular accounts and likes their photos based on a list of the most popular Hashtags. The technologies used are a custom Instagram API, Python, and a Selenium browser.
+                    </p>
+                    </h3>
+                    <h3> {ebrius}
+                        <p>
+                            ‘Ebrius’ is an application that allows one to showcase their bartending chops by allowing you to post instructions to create a drink and allow others to recreate and rate it.
+                            The technologies used are Express, MySQL, jQuery, JavaScript
+                    </p>
+                    </h3>
+                    <h3> {gamblr}
+                        <p>
+                            ‘Gamblr’ gives a user a secure, fun, and easy way to place bets on live E-sporting events.
+                            The technologies uses are React, Socket.io, MongoDB, Express.js, Node.js
+                    </p>
+                    </h3>
+                    <h3> {twitch}
+                        <p>
+                            'Ohwise1's Twitch Bot' gives a twitch streamer the ability to have a custom bot at their disposal.
+                            Using the Twitch API it pulls data from the channels chat, upon hearing a specific command the bot will respond with different functions.
                     </p>
                     </h3>
                 </div>
